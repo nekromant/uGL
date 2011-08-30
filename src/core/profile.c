a1e158566f5ac31acd15cc2ba208d246656b98d4
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2011-08-30 16:33:26 +0400

    core: reworked the profiler a bit.

--- src/core/profile.c
+++ src/core/profile.c
@@ -7,6 +7,8 @@
 #include <stdio.h>      // Header file for printf debugging.
 #include <stdarg.h>
 #include <time.h>
+#include <sys/time.h>
+
 #include "console.h"
 #include "common.h"
 #include "frametimer.h"
@@ -14,46 +16,52 @@
 
 struct ugl_profile_t* ugl_profile_create(int count, char* tag)
 {
-  struct ugl_profile_t* prof = (struct ugl_profile_t*) malloc(2*sizeof(int)+sizeof(char*)(count+1)*sizeof(struct timeval));
-  prof->pos=0;
-  prof->tag=tag;
-  prof->count=count;
-  gettimeofday(&prof->start, NULL);
+  struct ugl_profile_t* prof = (struct ugl_profile_t*) malloc(
+    sizeof(struct ugl_profile_header_t) + count*sizeof(struct ugl_profile_stamp_t));
+  prof->h.pos=0;
+  prof->h.tag=tag;
+  prof->h.count=count;
+  gettimeofday(&prof->h.start, NULL);
   return prof;
 }
 
-__inline ugl_profile_mark(struct ugl_profile_t* prof)
+__inline ugl_profile_mark(struct ugl_profile_t* prof, char* label)
 {
-  gettimeofday(&prof->data[prof->pos++], NULL);
+  gettimeofday(&prof->data[prof->h.pos].stamp, NULL);
+  prof->data[prof->h.pos++].tag=label;
 }
 
 void ugl_profile_print(struct ugl_profile_t* prof)
 {
-  printf("UGL PROFILER REPORT\n");
+  printf("UGL PROFILER REPORT: %s\n", prof->h.tag);
   printf("---\n");
   int i;
-  struct timeval prev = prof->start;
+  struct timeval prev = prof->h.start;
   float delta, abs;
-  for (i=0; i < prof->count; i++ )
+  for (i=0; i < prof->h.count; i++ )
   {
-    delta = (float)(prof->data[i].tv_sec-prev.tv_sec) + 0.000001*((float)(prof->data[i].tv_usec-prev.tv_usec));
-    abs = (float)(prof->data[i].tv_sec-prof->start.tv_sec) + 0.000001*((float)(prof->data[i].tv_usec-prof->start.tv_usec));
-    prev = prof->data[i];
-    printf("[%.6f] %.6f\n",abs,delta);
+    delta = (float)(prof->data[i].stamp.tv_sec-prev.tv_sec) + 0.000001*((float)(prof->data[i].stamp.tv_usec-prev.tv_usec));
+    abs = (float)(prof->data[i].stamp.tv_sec-prof->h.start.tv_sec) + 0.000001*((float)(prof->data[i].stamp.tv_usec-prof->h.start.tv_usec));
+    prev = prof->data[i].stamp;
+    printf("[%.6f] %.6f (%s)\n",abs,delta,prof->data[i].tag);
   }
 }
 
 void ugl_profiler_test()
 {
-  struct ugl_profile_t* p = ugl_profile_create(10);
-  ugl_profile_mark(p);
+  struct ugl_profile_t* p = ugl_profile_create(10,"test profiling");
+  ugl_profile_mark(p,"uisn");
   sleep(1);
-  ugl_profile_mark(p);
-  ugl_profile_mark(p);
-  ugl_profile_mark(p);
-  sleep(5);
-  ugl_profile_mark(p);
-  ugl_profile_mark(p);
+  ugl_profile_mark(p,"uisn");
+  sleep(2);
+  ugl_profile_mark(p,"uin");
+  sleep(3);
+  ugl_profile_mark(p,"uins");
+  ugl_profile_mark(p,"uindfa");
+  ugl_profile_mark(p,"uin");
+  ugl_profile_mark(p,"uindfa");
+  ugl_profile_mark(p,"uin");
+  ugl_profile_mark(p,"uindfa");
   ugl_profile_print(p);
   exit(0);
   
--- src/core/profile.h
+++ src/core/profile.h
@@ -4,13 +4,19 @@ struct ugl_profile_stamp_t
   char* tag;
 };
 
-struct ugl_profile_t
+struct ugl_profile_header_t
 {
   struct timeval start;
   int pos;
   int count;
-  char* tag
-  struct timeval data[];
+  char* tag;
+};
+
+struct ugl_profile_t
+{
+  struct ugl_profile_header_t h;
+  //struct timeval data[];
+  struct ugl_profile_stamp_t data[];
 };
 
 #ifdef UGL_PROFILE_ENABLED
